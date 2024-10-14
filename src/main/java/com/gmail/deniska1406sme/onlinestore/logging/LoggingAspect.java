d8868692f0b548571c11fca975551c9b04c7303b
Denys Smelnytskyi <deniska1406sme@gmail.com>
2024-10-14 21:01:51 +0300

    Added checking for sensitive data and its processing during logging

--- src/main/java/com/gmail/deniska1406sme/onlinestore/logging/LoggingAspect.java
+++ src/main/java/com/gmail/deniska1406sme/onlinestore/logging/LoggingAspect.java
@@ -20,16 +20,37 @@ public class LoggingAspect {
 
     @Before("execution(* com.gmail.deniska1406sme.onlinestore..*(..))")
     public void logBefore(JoinPoint joinPoint) {
-        logger.info("Method call: {} with arguments: {}", joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
+        Object[] args = joinPoint.getArgs();
+        Object[] filteredArgs = new Object[args.length];
+        for (int i = 0; i < args.length; i++) {
+            filteredArgs[i] = checkSensitiveData(args[i]);
+        }
+        logger.info("Method call: {} with arguments: {}", joinPoint.getSignature().getName(), Arrays.toString(filteredArgs));
     }
 
     @AfterReturning(pointcut = "execution(* com.gmail.deniska1406sme.onlinestore..*(..))",returning = "result")
     public void logAfter(JoinPoint joinPoint, Object result) {
-        logger.info("Method {} completed successfully, with result: {}", joinPoint.getSignature().getName(), result);
+        Object filteredResult = checkSensitiveData(result);
+        if ("createToken".equals(joinPoint.getSignature().getName()) || "authenticate".equals(joinPoint.getSignature().getName())
+                || "login".equals(joinPoint.getSignature().getName())) {
+            logger.info("Method {} completed successfully, but result is hidden.", joinPoint.getSignature().getName());
+            return;
+        }
+        logger.info("Method {} completed successfully, with result: {}", joinPoint.getSignature().getName(), filteredResult);
     }
 
     @AfterThrowing(pointcut = "execution(* com.gmail.deniska1406sme.onlinestore..*(..))", throwing = "exception")
     public void logAfterThrowing(JoinPoint joinPoint, Throwable exception) {
         logger.error("Method {} threw an exception: {}", joinPoint.getSignature().getName(), exception.getMessage());//, exception) for extended stack trace
+    }
+
+    private Object checkSensitiveData(Object arg){
+        if (arg instanceof String) {
+            String strArg = (String) arg;
+            if (strArg.matches(".*password.*") || strArg.matches(".*token.*")) {
+                return "****";
+            }
+        }
+        return arg;
     }
 }
